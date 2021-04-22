c07c22add1bdb35a8ceddad5885ae69a49ae45cb
Sharath Kumar <sharathkumar.vatti@innovapptive.com>
2021-04-22 17:54:32 +0530

    Populated details of random meal addded search component

--- FoodyCookBook/ViewController.swift
+++ FoodyCookBook/ViewController.swift
@@ -9,16 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var mealImage: UIImageView!
+    lazy var searchbar = UISearchBar(frame: CGRect.zero)
+
     var loader:UIAlertController?
     var randomMeal:MealObject?
     override func viewDidLoad() {
         super.viewDidLoad()
+        addSearchIcon()
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 44
+        tableView.sectionHeaderHeight = 44
+        tableView.tableFooterView = UIView()
     }
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         getRandomMeal()
     }
+    func addSearchIcon() {
+        let search = UIBarButtonItem(image: UIImage(systemName: "magnifyingglass"), style: .plain, target: self, action: #selector(handleSearchAction))
+        self.navigationItem.rightBarButtonItem = search
+        self.navigationItem.titleView = nil
+        self.navigationItem.title = "Foody Cook Book"
+
+    }
+    @objc func handleSearchAction() {
+        searchbar.placeholder = "Search"
+        searchbar.searchTextField.returnKeyType = .search
+        searchbar.delegate = self
+        searchbar.showsCancelButton = true
+        navigationItem.titleView = searchbar
+        self.navigationItem.rightBarButtonItem = nil
+
+
+    }
+    
     func prepareData(for meal:MealObject) {
         if let url = URL(string: meal.strMealThumb ?? "") {
             downloadImage(from: url)
@@ -85,9 +111,68 @@ extension ViewController {
             // always update the UI from the main thread
             DispatchQueue.main.async() { [weak self] in
                 self?.mealImage.image = UIImage(data: data)
+                self?.tableView.reloadData()
             }
         }
     }
 
 }
 
+extension ViewController:UITableViewDelegate,UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        if let meal = self.randomMeal {
+            let mirror = Mirror(reflecting: meal)
+            return mirror.children.count-1
+        }
+        return 0
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.backgroundColor = .lightGray
+        cell.textLabel?.numberOfLines = 0
+        if let meal = self.randomMeal {
+            let mirror = Mirror(reflecting: meal).children
+            let filteredLabels = mirror.map({$0.value})[0..<mirror.count-1] ///eliminating  thumbnail
+            cell.textLabel?.text = filteredLabels[indexPath.section] as? String ?? ""
+
+        }
+        return cell
+    }
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let header = UILabel(frame: CGRect(x: 5, y: 0, width: tableView.frame.width - 2*5, height: 44))
+        header.textAlignment = .left
+        header.textColor = .gray
+        if let meal = self.randomMeal {
+            let mirror = Mirror(reflecting: meal).children
+            let filteredLabels = mirror.map({$0.label})[0..<mirror.count-1] ///eliminating  thumbnail
+            let value = filteredLabels[section]
+            if let range = value?.range(of: "str") { //remove str in the starting
+                header.text = filteredLabels[section]?.replacingCharacters(in: range, with: "")
+
+            }
+
+        }
+        
+        return header
+    }
+    
+}
+extension ViewController:UISearchBarDelegate{
+    func searchBarShouldBeginEditing(_ searchBar: UISearchBar) -> Bool {
+        let cancel = UIBarButtonItem(title: "Cancel", style: UIBarButtonItem.Style.plain, target: self, action:#selector(searchBarCancelButtonClicked(_:)))
+        self.navigationItem.setRightBarButton(cancel, animated: true)
+        return true
+    }
+
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+            
+    }
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchbar.text?.removeAll()
+        addSearchIcon()
+    }
+
+}
